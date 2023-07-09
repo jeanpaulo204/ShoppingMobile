06b7b6620d44bb65ed9532193624d35aaf7d6e02
Moises Melo Viana Ferreira <moisesmvferreira@gmail.com>
2023-07-08 21:18:27 -0300

    alterações

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>teste</Text>
       <StatusBar style="auto" />
+      <View style={styles.batata}>
+        <Text style={styles.texto}><strong>Login</strong></Text>
+        <TextInput style={styles.input} placeholder="alguma coisa "/> 
+      </View>
     </View>
+    
   );
 }
 
@@ -17,4 +21,18 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  batata: {
+    height: 250,
+    width: 250,
+    backgroundColor:  'red',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  texto: {
+    flex: 1,
+    fontSize: '23px',
+  },
+  input: {
+    backgroundColor: 'white',
+  }
 });
