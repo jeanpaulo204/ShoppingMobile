ede84b72f553b934f04d368cd69db6446c079ac2
Moises Melo Viana Ferreira <moisesmvferreira@gmail.com>
2023-07-08 21:56:55 -0300

    component

---
+++ components/input.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, Text, View, TextInput, Button }
+
+const Input = () => {
+    return (
+       <View>
+            <Text>Component</Text>
+       </View>
+        );
+  };
+
+export default Input;
