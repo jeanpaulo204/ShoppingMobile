1b4c845548cb221b47ce2176eff020c9a47c3808
Jean Paulo <jean.paulo@edu.itajai.sc.gov.br>
2023-07-14 15:56:22 -0300

    feat: add transition

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { createStackNavigator, TransitionSpecs } from '@react-navigation/stack';
 import HomeScreen from './rotas/HomeScreen';
 import DetailsScreen from './rotas/DetailsScreen';
 import CadastroScreen from './rotas/Cadastro';
@@ -9,28 +9,44 @@ import PainelDePedido from './rotas/PainelDePedido';
 
 const Stack = createStackNavigator();
 
+const transitionConfig = {
+  animation: 'spring',
+  config: {
+    stiffness: 500,
+    damping: 50,
+    mass: 3,
+    overshootClamping: true,
+    restDisplacementThreshold: 0.01,
+    restSpeedThreshold: 0.01,
+  },
+};
+
 function App() {
   return (
-    <NavigationContainer  >
+    <NavigationContainer>
       <Stack.Navigator
-      screenOptions={{
-        headerMode: 'screen',
-        headerTintColor: 'white',
-        headerStyle: { backgroundColor: '#A659BF' },
-      }}
-      
+        screenOptions={{
+          headerMode: 'screen',
+          headerTintColor: 'white',
+          headerStyle: { backgroundColor: '#A659BF' },
+          transitionSpec: {
+            open: transitionConfig,
+            close: transitionConfig,
+          },
+        }}
       >
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Details" component={DetailsScreen} />
         <Stack.Screen name="Cadastro" component={CadastroScreen} />
-        <Stack.Screen cardOverlayEnabled name="Recuperar Senha" component={RecuperarSenha} />
+        <Stack.Screen
+          name="Recuperar Senha"
+          component={RecuperarSenha}
+          options={{ cardOverlayEnabled: true }}
+        />
         <Stack.Screen name="Painel De Pedido" component={PainelDePedido} />
       </Stack.Navigator>
     </NavigationContainer>
-   
   );
 }
-
-
 
 export default App;
